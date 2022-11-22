9f397433f8df66e8962061600e0f7bea6ce7432b
Dalai <edalai0115@gmail.com>
2022-11-22 15:13:46 +0800

    list delete ajilliilaw

--- src/main/java/com/example/foodrecipes/Controller.java
+++ src/main/java/com/example/foodrecipes/Controller.java
@@ -4,11 +4,14 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -16,6 +19,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Controller {
+    @FXML
+    private HBox ingredients;
+    @FXML
+    private ScrollPane scrollPane;
 
     @FXML
     private Button add;
@@ -23,11 +30,17 @@ public class Controller {
     private Button login_btn;
     @FXML
     private Button find_btn;
+    private boolean spAdded = false;
 
+    @FXML
+    void test(ActionEvent event) {
+        System.out.println(selected);
+    }
     @FXML
     void onFindClicked(ActionEvent event) {
 
     }
+    ArrayList<Node> selected;
 
     @FXML
     void onOpenLogin(ActionEvent event) throws IOException {
@@ -43,11 +56,82 @@ public class Controller {
         stage.setScene(scene);
         stage.showAndWait();
     }
+    private boolean isInList(String id) {
 
+        for (int i = 0; i < ingredients.getChildren().size(); i++) {
+            if (ingredients.getChildren().get(i).getId().equals(id)) return true;
+        }
+        return false;
+    }
+    public EventHandler<MouseEvent> mouseEnter = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent e) {
+
+            HBox hbox = (HBox)e.getSource();
+
+            hbox.setStyle("-fx-border:1px;-fx-border-color:rgb(255, 102, 102);-fx-border-radius:10px;" +
+                    "-fx-background-color:rgb(255, 102, 102);");
+            // hbox.removeEventFilter(MouseEvent.MOUSE_CLICKED, this);
+        }
+    };
+    public EventHandler<MouseEvent> mouseExit = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent e) {
+
+            HBox hbox = (HBox)e.getSource();
+
+            hbox.setStyle("-fx-border:1px;-fx-border-color:#E58412;-fx-border-radius:10px;" +
+                    "-fx-background-color:white;");
+
+            // hbox.removeEventFilter(MouseEvent.MOUSE_CLICKED, this);
+        }
+    };
+    public EventHandler<MouseEvent> mouseClick = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent e) {
+
+            HBox hbox = (HBox)e.getSource();
+            ingredients.getChildren().remove(hbox);
+            selected.remove(hbox);
+
+
+            //System.out.println(hbox.getParent().getChildrenUnmodifiable().toString());
+            // hbox.removeEventFilter(MouseEvent.MOUSE_CLICKED, this);
+        }
+    };
     @FXML
     void addIngredient(ActionEvent event) throws IOException {
-        ArrayList<Node> selected = onOpenDialog();
+        selected = onOpenDialog();
         System.out.println(selected);
+        if(!spAdded) {
+            scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+            spAdded=true;
+        }
+
+        for (Node ing : selected) {
+
+            String id = ing.getId();
+
+
+            if (!isInList(id)) {
+//                ing.minWidth(300);
+//                ing.prefWidth(300);
+//                ing.maxWidth(300);
+//                ing.setStyle("-fx-padding: 2px;");
+
+
+//                comp.addUserIngredient(ing.getId());
+                ing.addEventFilter(MouseEvent.MOUSE_ENTERED,mouseEnter);
+                ing.addEventFilter(MouseEvent.MOUSE_EXITED,mouseExit);
+                ing.addEventFilter(MouseEvent.MOUSE_CLICKED,mouseClick);
+
+                ingredients.getChildren().add(ing);
+
+            } else System.out.println("Item is in list");
+
+            scrollPane.setContent(ingredients);
+
+        }
 
     }
     @FXML
