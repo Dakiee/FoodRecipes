8531c0537daa9caac52dd4f561f8ab21c63277c1
Dakiee <ldavaanyam@gmail.com>
2022-11-23 11:58:29 +0800

    register done

--- src/main/java/com/example/foodrecipes/LoginController.java
+++ src/main/java/com/example/foodrecipes/LoginController.java
@@ -2,9 +2,16 @@ package com.example.foodrecipes;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -52,8 +59,27 @@ public class LoginController {
             }
 
         } catch(Exception e){
-
+            e.printStackTrace();
+            e.getCause();
         }
     }
 
+    public void createAccountForm(MouseEvent mouseEvent) {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Register.fxml"));
+            Parent root = fxmlLoader.load();
+
+            Scene scene = new Scene(root, 800, 570);
+            Stage stage = new Stage();
+            stage.setMinWidth(600);
+            stage.setMinHeight(600);
+
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(scene);
+            stage.showAndWait();
+        }catch(Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
 }
---
+++ src/main/java/com/example/foodrecipes/RegisterController.java
@@ -0,0 +1,64 @@
+package com.example.foodrecipes;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.File;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+public class RegisterController{
+
+    @FXML
+    private Button btnSignUp;
+
+    @FXML
+    private TextField tfUsername;
+    @FXML
+    private Label lblIsValid;
+    @FXML
+    private PasswordField setPasswordField;
+    @FXML
+    private Label lblRegistrationMessage;
+    @FXML
+    private PasswordField confirmPasswordField;
+
+    @FXML
+    void signUpEvent(ActionEvent event) {
+        if(setPasswordField.getText().equals(confirmPasswordField.getText())){
+            registerUser();
+            lblIsValid.setText("");
+        }
+        else{
+            lblIsValid.setText("Password does not match");
+        }
+    }
+
+    public void registerUser(){
+        DataBaseConnection connectNow = new DataBaseConnection();
+        Connection connectDB = connectNow.getConnection();
+
+        String username = tfUsername.getText();
+        String password = setPasswordField.getText();
+
+        String insertFields = "INSERT INTO user_account(username, PASSWORD) VALUES ('";
+        String insertValues = username + "','" + password + "')";
+        String insertToRegister = insertFields + insertValues;
+
+        try{
+            Statement statement = connectDB.createStatement();
+            statement.executeUpdate(insertToRegister);
+
+            lblRegistrationMessage.setText("Success modofaka");
+        }catch(Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+}
